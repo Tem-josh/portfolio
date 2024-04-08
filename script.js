062c4bd87697211cc258f4c9c278dbfe8177eaed
Tem-josh <jhoshuatope@gmail.com>
2024-04-08 19:17:36 +0100

    updated 8-04-24

--- script.js
+++ script.js
@@ -44,3 +44,32 @@ document.addEventListener("DOMContentLoaded", function () {
   // Set the interval for changing slides, adjusted to match the CSS transition
   setInterval(nextSlide, slideInterval);
 });
+
+(function(){
+  emailjs.init("yaaJLsSp1xnF01PN8");
+})();
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Get form values
+  var form = this;
+  var userEmail = form.user_email.value;
+  var userMessage = form.message.value;
+
+  console.log(userEmail, userMessage);
+
+  // Send email using EmailJS
+  emailjs.send("service_w5np09k", "template_port", {
+      to_email: userEmail,
+      message_html: userMessage
+  }).then(function(response) {
+      console.log("Email sent successfully", response);
+      // Optionally, show a success message to the user
+  }, function(error) {
+      console.error("Email send failed", error);
+      // Optionally, show an error message to the user
+  });
+
+  // Clear form fields
+  form.reset();
+});
